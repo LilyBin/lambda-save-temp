b008898948ec967e2f59f330ae451ed0ddaec795
Timothy Gu <timothygu99@gmail.com>
2016-09-30 23:39:30 -0700

    Use Node.js v4 runtime and ES2015 features

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
+'use strict';
+
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const TMP = 'lilybin-source-files';
 
-exports.handler = function(event, context) {
-    var t = process.hrtime();
-    var id = Date.now() + '-' + Math.random().toString(36).substr(2);
+exports.handler = function(event, context, callback) {
+    let id = `${Date.now()}-${Math.random().toString(36).substr(2)}`;
+
     s3.putObject({
         Bucket: TMP,
-        Key: id + '.ly',
+        Key: `${id}.ly`,
         Body: event.code,
         ContentType: 'text/plain',
         StorageClass: 'REDUCED_REDUNDANCY'
     }, function (err) {
         if (err) {
-            console.error(id, 'Errored')
-            console.error(id, err)
-            console.error(id, err.message)
-            console.error(id, err.stack)
-            return context.fail('Failed to upload score: ' + err.message)
+            console.error(id, 'Errored');
+            console.error(id, err);
+            console.error(id, err.message);
+            console.error(id, err.stack);
+            err.message = `Failed to upload score: ${err.message}`;
+            return callback(err);
         }
-        context.succeed({id: id})
-        console.log(id, 'Done')
+        console.log(id, 'Done');
+        callback(null, {id});
     });
 };
